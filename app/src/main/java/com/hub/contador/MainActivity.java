c689cac6af666ca46440516f5ef1395bc157f224
Deretti <juliocesarderetti@gmail.com>
2024-09-09 21:58:39 -0300

    Second

--- app/src/main/java/com/hub/contador/MainActivity.java
+++ app/src/main/java/com/hub/contador/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import java.util.Random;
 
 public class MainActivity extends Activity{
@@ -17,12 +19,20 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
         button.setText("Yametekudasai");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Button b  = (Button) view;
-                b.setText(Integer.toString(randomico.nextInt(++lContador)));
+
+                TextView dadosInicial = findViewById(R.id.editTextNumber2);
+                TextView dadosFinal   = findViewById(R.id.editTextNumber);
+
+                int iInicial = Integer.parseInt(dadosInicial.getText().toString());
+                int iFinal   = Integer.parseInt(dadosFinal.getText().toString());
+
+                textView.setText(Integer.toString(randomico.nextInt(iFinal - iInicial) + iInicial));
             }
         });
     }
